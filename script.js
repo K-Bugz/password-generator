4324822763bad73802716a2c87d791e4e9261df1
Kevin Bugusky <kbugusky@gmail.com>
2022-01-14 16:52:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,19 @@
 // Global Variables
 const lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-const possibleList = [] // a blank list to pull from. 
-var passLength;
-// Below are the undefined bools that will return false
-var confirmUpperCase;
-var confirmlowerCase;
-var confirmNums;
-var confirmSpecialChars;
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+let possibleList = [] // a blank list to pull from. 
+const password = []; // populate this from the possiblelist
+var passLength; // password length
+// Below are the undefined bools set to false at the start
+var confirmUpperCase = false;
+var confirmlowerCase = false;
+var confirmNums = false;
+var confirmSpecialChars = false;
+// Get references to the #generate element  
+var generateBtn = document.querySelector("#generate"); // selects the generate button
+var pword = document.querySelector("#password"); // selects the "Your Secure Password"
 
 // Write password to the #password input
 function writePassword() {
@@ -24,29 +26,43 @@ function writePassword() {
     // Conditionals to ensure password is an appropriate length. 
     if (!passLength) { // parseInt returns nothing or falesy if not an int 
         alert("Last chance! Pick a value that is a number")
-    } else if (passLength <= 8 || passLength >= 128) {
+    } else if (passLength < 8 || passLength > 128) {
         passLength = parseInt(prompt("Last chance! Pick password length from 8 to 128"));
     } else {
-        // Parameter confirmation
+        // Parameter confirmation and pushes the arrays together. 
         confirmUpperCase = confirm("Do you want uppercase letters? (click OK for yes)");
+        if (confirmUpperCase) {
+            // possibleList.push(upperLetters)
+            possibleList = possibleList.concat(upperLetters);
+        }
         confirmlowerCase = confirm("Do you want lowercase letters? (click OK for yes)");
+        if (confirmlowerCase) {
+            // possibleList.push(lowerLetters)
+            possibleList = possibleList.concat(lowerLetters);
+        }
         confirmNums = confirm("Do you want numbers included? (click OK for yes)");
-        confirmSpecialChars = confirm("Do you want special characters included? (click OK for yes)");
+        if (confirmNums) {
+            // possibleList.push(nums)
+            possibleList = possibleList.concat(nums);
+        }
+        confirmSpecialChars = confirm("Do syou want special characters included? (click OK for yes)");
+        if (confirmSpecialChars) {
+            // possibleList.push(specialChars)
+            possibleList = possibleList.concat(specialChars);
+        }
+        // Paramenters set, lets generate a password!
+        for (var i = 0; i < passLength; i++) {
+            password.push(possibleList[Math.floor(Math.random() * possibleList.length)]);
+        }
+        var ps = password.join("");
+        pword.value = ps;
     };
 
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
-    passwordText.value = password;
 
 
-    // Paramenters set, lets generate a password! (fix)
-    for (var i = 0; i < passLength; i++) {
-        var passwordOptions = choices[Math.floor(Math.random() * choices.length)];
-        password.push(pickChoices);
-    }
+
 }
 
 
-// Add event listener to generate button
+// Add event listener to generate button that calls write password
 generateBtn.addEventListener("click", writePassword);
